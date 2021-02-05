44045144e3d023b59c66a52628d7b2d234d1a4f6
Robin Jahan <61151683+RobinJahan@users.noreply.github.com>
2021-02-05 10:35:48 +0100

    update

--- TP1/Entities/Author.cs
+++ TP1/Entities/Author.cs
@@ -1,10 +1,12 @@
+using ConsoleApp1.Database.Contracts;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TP1.Entities
 {
